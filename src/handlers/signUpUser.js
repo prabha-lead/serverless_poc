d11bbc1e2b6b7581cadf9e4e847995b90ed925aa
prabha-lead <prabakarsece@gmail.com>
2023-07-12 18:02:21 +0530

    uploaded to s3 bucket completed

--- src/handlers/signUpUser.js
+++ src/handlers/signUpUser.js
@@ -3,16 +3,31 @@
 const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const sqs = new AWS.SQS();
+const s3 = new AWS.S3();
 
 module.exports.signUpUser = async (event) => {
   try {
-    const { name, email, password } = JSON.parse(event.body);
+    const { name, email, password, image } = JSON.parse(event.body);
 
     // Generate a unique user ID
     const userId = Math.random().toString(36).substring(2, 15);
 
     // Generate a verification code
     const verificationCode = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
+
+    // Generate a unique file name
+    const fileName = `users/${userId}.jpg`;
+
+    // Upload user image to S3
+    const uploadParams = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: fileName,
+      Body: Buffer.from(image, 'base64'), // if image is Base64 encoded
+      ContentType: 'image/jpeg'
+    };
+
+    const uploadResponse = await s3.upload(uploadParams).promise();
+    const imageUrl = uploadResponse.Location;
 
     // Create a new user record in DynamoDB
     const params = {
@@ -22,6 +37,7 @@ module.exports.signUpUser = async (event) => {
         name,
         email,
         password,
+        imageUrl,
         isWelcomeSend: false,
         verificationCode,  // Store verification code
       },
@@ -45,7 +61,7 @@ module.exports.signUpUser = async (event) => {
       statusCode: 200,
       body: JSON.stringify({
         message: 'User signed up successfully! A verification code has been sent to your email.',
-        userId,
+        userId
       }),
     };
   } catch (error) {
